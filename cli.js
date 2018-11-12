305f2f85d9f740d6a2ac8c7f809ce73a64597474
Thomas Springborg <>
2018-11-12 15:25:20 +0100

    more setup

--- cli.js
+++ cli.js
@@ -7,7 +7,7 @@ const argv = minimist(process.argv.slice(2))
 // In a custom directory or current directory
 const targetPath = path.resolve(argv._[0] || '.')
 
-console.log(`> Generating Nuxt.js project in ${targetPath}`)
+console.log(`> Generating mola-nuxt-app in ${targetPath}`)
 
 // See https://sao.js.org/#/advanced/standalone-cli
 sao({
