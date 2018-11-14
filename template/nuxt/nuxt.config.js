643ee2c91eb7661c12e6fdcb3abf16ca82cd671b
Thomas Springborg <>
2018-11-14 14:55:38 +0100

    added dotenv

--- template/nuxt/nuxt.config.js
+++ template/nuxt/nuxt.config.js
@@ -1,71 +1,75 @@
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+    mode: 'universal',
 
-  /*
-  ** Headers of the page
-  */
-  head: {
-    title: pkg.name,
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+    /*
+    ** Headers of the page
+    */
+    head: {
+        title: pkg.name,
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: pkg.description }
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        ]
+    },
+
+    /*
+    ** Customize the progress-bar color
+    */
+    loading: { color: '#fff' },
+
+    /*
+    ** Global CSS
+    */
+    css: [
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
 
-  /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+    /*
+    ** Plugins to load before mounting the App
+    */
+    plugins: [
+    ],
 
-  /*
-  ** Global CSS
-  */
-  css: [
-  ],
-
-  /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-  ],
-
-  /*
-  ** Nuxt.js modules
-  */
-  modules: [
+    /*
+    ** Nuxt.js modules
+    */
+    modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios'
-  ],
-  /*
-  ** Axios module configuration
-  */
-  axios: {
-    // See https://github.com/nuxt-community/axios-module#options
-  },
-
-  /*
-  ** Build configuration
-  */
-  build: {
+    ],
     /*
-    ** You can extend webpack config here
+    ** Axios module configuration
     */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/
-        })
-      }
+    axios: {
+    // See https://github.com/nuxt-community/axios-module#options
+    },
+
+    /*
+    ** Build configuration
+    */
+    build: {
+        /*
+        ** You can extend webpack config here
+        */
+        extend(config, ctx) {
+            // fix 'fs' is undefined (https://github.com/nuxt-community/dotenv-module/issues/11)
+            config.node = {
+                fs: 'empty',
+            }
+            // Run ESLint on save
+            if (ctx.isDev && ctx.isClient) {
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/
+                })
+            }
+        }
     }
-  }
 }
